720030836ca4dfa0970088cd5adc958c198af510
HiroseKakeru <hirose_kakeru@retail-ai.jp>
2023-04-11 14:42:03 +0900

    fix domain

--- src/Callback.tsx
+++ src/Callback.tsx
@@ -41,7 +41,7 @@ export const Callback = () => {
           chainId: "0x13881",
         },
         web3AuthNetwork: "testnet",
-        useCoreKitKey: true,
+        useCoreKitKey: false,
       });
 
       const openloginAdapter = new OpenloginAdapter({
@@ -106,7 +106,7 @@ export const Callback = () => {
           // domain: "https://shahbaz-torus.us.auth0.com",
           verifierIdField: "sub",
           id_token: query.get("token"),
-          domain: "https://hirosekakeru.github.io/"
+          domain: "https://auth-dev.su-pay.jp/"
           // connection: "google-oauth2", // Use this to skip Auth0 Modal for Google login.
         },
       }
